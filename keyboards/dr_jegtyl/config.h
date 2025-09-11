0430280614cbf1bf8ab30fa9b0385203257bd31b
Jens Eger <7154003+JensEger@users.noreply.github.com>
2025-09-11 21:44:57 +0200

    Added some comments for future investigations

--- keyboards/dr_jegtyl/config.h
+++ keyboards/dr_jegtyl/config.h
@@ -2,6 +2,11 @@
 
 #pragma once
 
+// https://docs.qmk.fm/features/pointing_device
+// https://ryanis.cool/cosmos/docs/qmk-rp2040/#configuration
+// https://ryanis.cool/cosmos/_app/immutable/assets/lemon-pcb-pinout.vhb9kmvX.png
+
+
 // #include "keyboards/dr_jegtyl/vik/config.vik.pre.h"
 
 // #define EE_HANDS
@@ -18,8 +23,19 @@
 // #define VIK_GPIO_2       GP27
 // #define VIK_WS2812_DI_PIN GP2
 
+// Config from Docs - doesn't work:
 /* Trackball */
-#define SPI_DRIVER SPID1
+// #define SPI_DRIVER SPID0
+// #define SPI_SCK_PIN GP18
+// #define SPI_MISO_PIN GP20
+// #define SPI_MOSI_PIN GP19
+// #define POINTING_DEVICE_CS_PIN GP10
+// #define ROTATIONAL_TRANSFORM_ANGLE -30 // Optional: Rotates the trackball
+// #define POINTING_DEVICE_INVERT_X // Optional: Inverts trackball X
+
+
+/* Trackball  Config using VIK Pins - doesn't work*/
+#define SPI_DRIVER SPID0
 #define SPI_SCK_PIN GP14
 #define SPI_MISO_PIN GP12
 #define SPI_MOSI_PIN GP15
@@ -47,4 +63,4 @@
 #define NO_AUTO_SHIFT_NUMERIC 
 #define AUTO_SHIFT_ENTER
 
-// #include "keyboards/dr_jegtyl/vik/config.vik.post.h"
+#include "keyboards/dr_jegtyl/vik/config.vik.post.h"
